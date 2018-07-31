e0ab9390768ae2bfe4271e8d953681c883c6a6bc
Ben Hearsum <bhearsum@mozilla.com>
2018-07-31 12:15:02 -0400

    Make some improvements to the ship graph.

--- release-ship.mm
+++ release-ship.mm
@@ -1,20 +1,19 @@
 sequenceDiagram
     participant RelEng as Release Engineer
     participant RelMan as Release Manager
-    participant bm81 as buildbot-master81
     participant Ship It
-    participant hg as hg.mozilla.org
     participant tc as Taskcluster
     participant rw as Release Workers
     participant bw as Balrog Workers
     participant bouncew as Bouncer Workers
+    participant tsw as Treescript Workers
     participant shipitw as Shipit Worker
     participant Balrog
     participant Bouncer
+    participant hg as hg.mozilla.org
 
-    RelMan ->> RelEng: Publish Release
-    RelEng ->> bm81: Create Decision Task
-    bm81 ->> tc: Create Decision Task
+    RelMan ->> Ship It: Publish Release
+    Ship It ->> tc: Create Decision Task
     tc ->> tc: Create Ship Graph
     tc ->> bw: Schedule Ship in Balrog
     bw ->> Balrog: Schedule Ship in Balrog
@@ -25,5 +24,7 @@ sequenceDiagram
     tc ->> RelMan: Notify that updates are awaiting Signoff in Balrog
     RelEng ->> Balrog: Sign off on updates
     RelMan ->> Balrog: Sign off on updates
-    tc ->> rw: Bump in-tree version
-    rw ->> hg: Bump in-tree version
+    tc ->> tsw: Bump in-tree version & tag
+    tsw ->> hg: Bump in-tree version & tag
+    tc ->> rw: Verify Bouncer Alias'
+    rw ->> Bouncer: Verify Bouncer Alias'
