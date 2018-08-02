033d6fa3fa6b5899738a0b40e211bd705b2eb6a6
Ben Hearsum <bhearsum@mozilla.com>
2018-08-02 18:23:07 -0400

    Combine similar things together to simplify the graph

--- release-promote.mm
+++ release-promote.mm
@@ -20,37 +20,26 @@ sequenceDiagram
     tc ->> tc: Create Promote Graph
     tc ->> shipitw: Mark as Started
     shipitw ->> Ship It: Mark as Started
-    tc ->>+ rw: Make Source Package
+    tc ->>+ rw: Make Bits
     Note over tc,rw: release-source
-    rw -->>- tc: Source Package
-    tc ->> sw: Sign Source Package
+    Note over tc,rw: nightly-l10n
+    Note over tc,rw: repackage-l10n
+    Note over tc,rw: release-partner-repack
+    Note over tc,rw: release-partner-repack-repackage
+    Note over tc,rw: partials
+    rw -->>- tc: Bits
+    tc ->> sw: Sign Bits
     activate sw
     Note over tc,sw: release-source-signing
-    sw ->> ss: Sign Source Package
-    activate ss
-    ss -->> tc: Signed Source Package
-    deactivate sw
-    deactivate ss
-    tc ->>+ rw: Make L10N Repacks
-    Note over tc,rw: nightly-l10n
-    rw -->>- tc: L10N Repacks
-    tc ->> sw: Sign L10N Internals 
-    activate sw
     Note over tc,sw: nightly-l10n-signing
-    sw ->> ss: Sign L10N Internals
-    activate ss
-    ss -->> tc: Signed L10N Internals
-    deactivate sw
-    deactivate ss
-    tc ->>+ rw: Package L10N Builds
-    Note over tc,rw: repackage-l10n
-    rw -->>- tc: Packaged L10N Builds
-    tc ->> sw: Sign L10n Packages
-    activate sw
     Note over tc,sw: repackage-signing-l10n
-    sw ->> ss: Sign L10n Packages
+    Note over tc,sw: release-partner-repack-signing
+    Note over tc,sw: release-partner-repack-repackage-signing
+    Note over tc,sw: partials-signing
+    Note over tc,sw: release-generate-checksums-signing
+    sw ->> ss: Sign Bits
     activate ss
-    ss -->> tc: Signed L10N Packages
+    ss -->> tc: Signed Bits
     deactivate sw
     deactivate ss
     tc ->> as: Sign and Push Langpacks
@@ -61,58 +50,10 @@ sequenceDiagram
     AMO -->> tc: Signed Langpacks
     deactivate AMO
     deactivate as
-    tc ->>+ rw: Make Partner Repacks
-    Note over tc,rw: release-partner-repack
-    rw -->>- tc: Partner Repacks
-    tc ->> sw: Sign Partner Internals 
-    activate sw
-    Note over tc,sw: release-partner-repack-signing
-    sw ->> ss: Sign Partner Internals
-    activate ss
-    ss -->> sw: Signed Partner Internals
-    deactivate sw
-    deactivate ss
-    tc ->>+ rw: Package Partner Builds
-    Note over tc,rw: release-partner-repack-repackage
-    rw -->>- tc: Packaged Partner Builds
-    tc ->> sw: Sign Partner Packages
-    activate sw
-    Note over tc,rw: release-partner-repack-repackage-signing
-    sw ->> ss: Sign Partner Packages
-    activate ss
-    ss -->> tc: Signed Partner Packages
-    deactivate sw
-    deactivate ss
-    tc ->>+ rw: Make Partial Updates
-    Note over tc,rw: partials
-    rw -->>- tc: Partial Updates
-    tc ->> sw: Sign Partial Updates
-    activate sw
-    Note over tc,sw: partials-signing
-    sw ->> ss: Sign Partial Updates
-    activate ss
-    ss -->> tc: Signed Partial Updates
-    deactivate sw
-    deactivate ss
     tc ->> bm: Copy Bits to Candidates Dir
-    Note over tc,bm: beetmover, beetmover-l10n, beetmover-repackage, beetmover-source
+    Note over tc,bm: beetmover, beetmover-l10n, beetmover-repackage, beetmover-source, release-generate-checksums-beetmover
     activate bm
     bm ->> s3: Copy Bits to Candidates Dir
-    activate s3
-    deactivate bm
-    deactivate s3
-    tc ->> sw: Sign Checksums
-    activate sw
-    Note over tc,sw: release-generate-checksums-signing
-    sw ->> ss: Sign Checksums
-    activate ss
-    ss -->> tc: Signed Checksums
-    deactivate sw
-    deactivate ss
-    tc ->> bm: Copy Checksums to Candidates Dir
-    activate bm
-    Note over tc,bm: release-generate-checksums-beetmover
-    bm ->> s3: Copy Checksums to Candidates Dir
     activate s3
     deactivate bm
     deactivate s3
