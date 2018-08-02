4fbe8f6930183ec023011c45ead2bfcf68a897ee
Ben Hearsum <bhearsum@mozilla.com>
2018-08-02 18:31:00 -0400

    Remove kinds because they add unnecessary complexity.

--- kinds/release-source.mm
+++
@@ -1,7 +0,0 @@
-sequenceDiagram
-    participant rw as Release Workers
-    participant hg as hg.mozilla.org
-
-    rw ->>+ hg: Clone repository
-    hg -->>- rw: Cloned repository
-    rw ->> rw: Make source package
